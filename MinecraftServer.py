839b514212c7a3cea6bac22e8f39d835761c6c56
teekar2023 <sree23palla@gmail.com>
2021-11-06 19:36:18 -0400

    v1.1.0

--- MinecraftServer.py
+++ MinecraftServer.py
@@ -2,30 +2,75 @@ import os
 import sys
 import webbrowser
 import time
+import requests
 import pyautogui as kbm
 from threading import Thread
 from tkinter import *
-from tkinter.messagebox import showinfo
+from tkinter.messagebox import showerror, showinfo, showwarning
 from tkinter.messagebox import askyesno
 from tkinter.simpledialog import askinteger, askstring
 
 
 def ngrok():
-    os.chdir(f"{cwd}\\ServerFiles\\")
+    os.chdir(f"{cwd}\\ngrok\\")
     os.system(f"ngrok authtoken {authtoken}")
     os.system("start cmd")
     time.sleep(1)
     kbm.typewrite(f"cd {cwd}\n")
-    kbm.typewrite("cd ServerFiles\n")
+    kbm.typewrite(f"cd ngrok\n")
     kbm.typewrite("ngrok tcp 25565\n")
     pass
 
 
 cwd = os.getcwd()
+print("EasyMinecraftServer Version: 1.1.0")
 print(f"Current Working Directory: {cwd}")
-print("Minecraft Version: 1.17.1")
 root = Tk()
 root.withdraw()
+try:
+    url = "http://github.com/teekar2023/EasyMinecraftServer/releases/latest/"
+    r = requests.get(url, allow_redirects=True)
+    redirected_url = r.url
+    if redirected_url != "https://github.com/teekar2023/EasyMinecraftServer/releases/tag/v1.1.0":
+        showwarning(title="Update Available", message="An update is available. Please update to the latest version to "
+                                                      "use this program. None of your data will be lost.")
+        webbrowser.open(redirected_url)
+        sys.exit(0)
+    else:
+        pass
+except Exception as e:
+    print(f"Error while checking for updates: {e}")
+if not os.path.exists("C:\\Program Files\\Java\\jdk-17.0.1\\bin"):
+    install_jdk_ask = askyesno(title="JDK Required", message="Java Development Kit 17 Is Required! Would You Like To "
+                                                             "Install It Now?")
+    if install_jdk_ask:
+        os.chdir(f"{cwd}\\JDK\\")
+        os.system("start jdk-17_windows-x64_bin.exe")
+        pass
+    else:
+        showerror(title="JDK Required", message="Java Development Kit 17 Is Required! Please Install It And Restart "
+                                                "The Program!")
+        sys.exit(0)
+    pass
+else:
+    pass
+version_selection = askstring("Minecraft Server", "Enter the version you want to use! '1.8.9' or '1.12.2' or '1.16.5' "
+                                                  "or '1.17.1'")
+if version_selection == "1.8.9":
+    version = "1.8.9"
+    pass
+elif version_selection == "1.12.2":
+    version = "1.12.2"
+    pass
+elif version_selection == "1.16.5":
+    version = "1.16.5"
+    pass
+elif version_selection == "1.17.1":
+    version = "1.17.1"
+    pass
+else:
+    showerror("Minecraft Server", "You have entered an invalid version!")
+    sys.exit(0)
 port_forward_prompt = askyesno(title="Port Forwarded?", message="Is port 25565 port forwarded on your network? Press "
                                                                 "'NO' if you are not sure!")
 if port_forward_prompt:
@@ -54,11 +99,15 @@ else:
     pass
 ram_input = askinteger(title="Server RAM", prompt="How many mb of ram would you like in the server? Please enter an "
                                                   "integer number! Minimum Recommended: 2000")
-os.chdir(f"{cwd}\\ServerFiles\\")
-showinfo(title="Minecraft Server 1.17.11", message="Server will be created/started after closing this popup! Type "
-                                                   "'STOP' and press 'ENTER' in the console window to shutdown the "
-                                                   "server! Also, your server's ip can be found in the ngrok window "
-                                                   "next to 'Forwarding' and should follow the format '("
-                                                   "numbers).tcp.ngrok.io:(more_numbers)! Have Fun!")
+os.chdir(f"{cwd}\\ServerFiles-{version}\\")
+showinfo(title="Minecraft Server", message="Server will be created/started after closing this popup! Type "
+                                           "'STOP' and press 'ENTER' in the console window to shutdown the "
+                                           "server! Also, if you are using ngrok, your server's ip can be found in "
+                                           "the ngrok window "
+                                           "next to 'Forwarding' and should follow the format: '("
+                                           "numbers).tcp.ngrok.io:(more_numbers)! If you are not using ngrok "
+                                           "and are port forwarded, the server ip will be your computer's ip with the "
+                                           "port '25565'! Have Fun!")
 os.system(f"java -Xmx{ram_input}M -Xms{ram_input}M -jar server.jar nogui")
+time.sleep(1)
 sys.exit(0)
--- build.py
+++ build.py
@@ -2,5 +2,10 @@ import os
 from shutil import copytree
 
 cwd = os.getcwd()
-os.system("pyinstaller MinecraftServer.py")
-copytree(f"{cwd}\\ServerFiles\\", f"{cwd}\\dist\\MinecraftServer\\ServerFiles\\")
+os.system("pyinstaller -i mc.ico MinecraftServer.py")
+copytree(f"{cwd}\\ServerFiles-1.8.9\\", f"{cwd}\\dist\\MinecraftServer\\ServerFiles-1.8.9\\")
+copytree(f"{cwd}\\ServerFiles-1.12.2\\", f"{cwd}\\dist\\MinecraftServer\\ServerFiles-1.12.2\\")
+copytree(f"{cwd}\\ServerFiles-1.16.5\\", f"{cwd}\\dist\\MinecraftServer\\ServerFiles-1.16.5\\")
+copytree(f"{cwd}\\ServerFiles-1.17.1\\", f"{cwd}\\dist\\MinecraftServer\\ServerFiles-1.17.1\\")
+copytree(f"{cwd}\\ngrok\\", f"{cwd}\\dist\\MinecraftServer\\ngrok\\")
+copytree(f"{cwd}\\JDK\\", f"{cwd}\\dist\\MinecraftServer\\JDK\\")
